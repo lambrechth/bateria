8e34d1b19a4cfc154e0e0965af7b84dd334e3bc2
queixo minuscolo <lambrecht.daniel@escola.pr.gov.br>
2025-09-16 12:18:07 -0300

    aaaaaa

---
+++ aaaaa/script.js
@@ -0,0 +1,21 @@
+//criação das 
+const sons = {
+    'A' : 'boom.wav',
+    'S' : 'clap.wav',
+    'D' : 'hithat.wav',
+    'F' : 'hithat.wav',
+    'G' : 'kick.wav',
+    'H' : 'openhat.wav',
+    'I' : 'ride.wav',
+    'J' : 'snare.wav',
+    'K' : 'tink.wav',
+    'L' : 'tom.wav',
+}
+//função para criar um div para cada tecla 
+const criarDiv = (sons) =>{
+    div.classlist('key')
+    div.textcontent = texto
+}
+
+exibir(sons);
+document.getElementById('container')
